ea7e32bf32f2ee036fda683e546ddf3ca215153d
Valéria Parajara <valeriaparajara@gmail.com>
2015-06-18 10:34:18 -0300

    Update loan serializer with inclusion of the field created_at

--- app/serializers/loan_serializer.rb
+++ app/serializers/loan_serializer.rb
@@ -1,5 +1,5 @@
 class LoanSerializer < ActiveModel::Serializer
-  attributes :id, :friend_email, :friend_name, :loaned_item
+  attributes :id, :friend_email, :friend_name, :loaned_item, :created_at
 
   def id
     object.id.to_s
