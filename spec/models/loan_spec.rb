55c8ce9005ba674dd39b1dbf3f6adf8d0c238400
Valéria Parajara <valeriaparajara@gmail.com>
2015-07-09 18:09:04 -0300

    refactoring test loan model

--- spec/models/loan_spec.rb
+++ spec/models/loan_spec.rb
@@ -2,11 +2,13 @@ require "rails_helper"
 
 describe Loan do
   describe "when update" do
+    let(:user) { create :user, :access_token, :with_loans }
+
     context "when the item lent is returded" do
-      subject(:loan) { build :loan, :returned}
+      let(:loan) { user.loans.last }
 
       it "set returned_at" do
-        loan.save
+        loan.update_attributes(returned: true)
         expect(loan.returned_at).to eq(Date.today)
       end
     end
